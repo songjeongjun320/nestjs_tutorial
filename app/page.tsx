3889c29447f7a625c0bb07ff5a4fce05627b070f
Jun <songjeongjun320@gmail.com>
2024-08-09 00:39:33 -0700

    08/09/2024

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    create-next-app !create-next-app 으로 설치하면
+    <br />
+    1. 컴파일과 번들링이 자동을 된다 (webpack 과 babel)
+    <br />
+    2. 자동 리프레쉬 기능으로 수정하면 화면에 바로 반영된다.
+    <br />
+    3. 서버사이드 렌더링이 지원된다.
+    <br />
+    4. 스태틱 파일을 지원합니다.
+
+   </div>
+  );
+}
